545676c14dc09938528d0833c71d6f61c5125d05
reboot329 <fey14@pitt.edu>
2015-10-11 23:04:31 -0400

    Update Quiz.java

--- app/src/main/java/com/example/fey/cityquiz/Quiz.java
+++ app/src/main/java/com/example/fey/cityquiz/Quiz.java
@@ -9,31 +9,69 @@ public class Quiz {
 
     ArrayList<Question> QuestionList = new ArrayList<Question>();   //Question List
     int CorrectAnswered;        //How many questions are answered correctly for now.
-
+    int WrongAnswered;
+    int CurrentQuestionIndex;
 
     //constructor
     public Quiz()
     {
+        String test_q = "University of Pittsburgh is ";
+        String test_a1 = "Food";
+        String test_a2 = "Animal";
+        String test_a3 = "Vehicle";
+        String test_a4 = "University";
+        String text_c = "University";
+        Question test_question = new Question(test_q,test_a1,test_a2,test_a3,test_a4,text_c);
+        QuestionList.add(test_question);
 
+
+        CorrectAnswered = 0;
+        CurrentQuestionIndex = 1;
+        WrongAnswered = 0;
     }
+
+    public void AddQuiz(Question q)
+    {
+        QuestionList.add(q);
+
+        System.out.println("11111");
+    }
+
 
     //return true if no questions left
     public boolean isEmpty()
     {
-        return true;
+       if(CurrentQuestionIndex < QuestionList.size())
+       {
+           CurrentQuestionIndex++;
+           return false;
+       }
+       else
+       {
+           return true;
+       }
     }
 
-    public void GenerateQuiz()
+    //send question to users.
+    public Question ReturnQuiz()
     {
-        if(!isEmpty())
-        {
-            //send question to activity
-        }
-        else
-        {
-            //result page
-        }
 
+
+        return QuestionList.get(CurrentQuestionIndex-1);
+
+
+    }
+
+    public void CheckAnswer(String answer)
+    {
+       if(QuestionList.get(CurrentQuestionIndex-1).isAnswer(answer))
+       {
+           CorrectAnswered++;
+       }
+       else
+       {
+           WrongAnswered++;
+       }
 
     }
 
