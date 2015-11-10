b8f88ffb3f412448fc9cf5573e80e5f015280f5c
reboot329 <fey14@pitt.edu>
2015-11-09 22:16:31 -0500

    Update HomePage.java
    Add one button to go to leaderboard

--- app/src/main/java/com/example/fey/cityquiz/HomePage.java
+++ app/src/main/java/com/example/fey/cityquiz/HomePage.java
@@ -1,6 +1,6 @@
 package com.example.fey.cityquiz;
 
-
+import android.widget.Toast;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,20 +10,20 @@ import android.content.Intent;
 import android.app.Activity;
 import android.view.MenuInflater;
 import android.view.Menu;
-import android.util.Log;
 import android.content.SharedPreferences;
 import com.parse.Parse;
-import com.parse.ParseUser;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
-
-public class HomePage extends Activity /*implements OnClickListener*/ {
+public class HomePage extends Activity {
 
     //take quiz button
     Button button;
+    //boolean to determine if parse has already been initialized
     public static boolean parseIsInitialized = false;
-    boolean isRegistered;
 
 
     @Override
@@ -31,20 +31,36 @@ public class HomePage extends Activity /*implements OnClickListener*/ {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        // Enable Local Datastore.
+        //Take Quiz button
+        button = (Button) findViewById(R.id.button);
+
+        //Check internet connection
+        ConnectivityManager con=(ConnectivityManager)getSystemService(Activity.CONNECTIVITY_SERVICE);
+        boolean wifi=con.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnectedOrConnecting();
+        boolean internet=con.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnectedOrConnecting();
+        //This one checks both wifi and normal connection
+        if(wifi|internet)
+        {
+            Toast.makeText(getApplicationContext(),
+                    "There is connections", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),
+                    "No connection", Toast.LENGTH_LONG).show();
+        }
+
+        // Initializes Parse so that we can store and retrieve objects to Parse.
         if(!parseIsInitialized) {
             Parse.enableLocalDatastore(this);
             Parse.initialize(this, "2DRZJCltwfbJKH7cAcbkVNU2i2UFGq2uuDEIaxIK", "CEzdLzmlsCUSOhIM5r5spTz7ZTRjbVScAqS0dzU2");
             parseIsInitialized = true;
-            ParseUser.enableRevocableSessionInBackground();
         }
 
-
-
-
-        button = (Button) findViewById(R.id.button);
-
-       //This is all the onclick information for the 'Take Quiz' button
+        /**
+         * This function is called when the take quiz button is clicked.
+         * It checks to make sure the user has not taken the quiz today, then goes to the quiz page.
+         */
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 //Open sharedPreferences, which saves information between app loads
@@ -63,7 +79,7 @@ public class HomePage extends Activity /*implements OnClickListener*/ {
                 editor.commit();
 
                 //Check to see if the user has already taken the quiz today
-                if(false){//dateOfLastQuizTaken.equals(todaysDate)){     //SET THIS TO ALWAYS EQUAL FALSE IF YOU WANT TO RUN THE QUIZ MORE THAN ONCE PER DAY
+                if(dateOfLastQuizTaken.equals(todaysDate)){     //SET THIS TO ALWAYS EQUAL FALSE IF YOU WANT TO RUN THE QUIZ MORE THAN ONCE PER DAY
                     button.setText("You've already taken the quiz today. Come back tommorow!");
                     button.setTextSize(20);
                 }
@@ -75,18 +91,25 @@ public class HomePage extends Activity /*implements OnClickListener*/ {
             }
         });
 
-        //when clicked, will take the user to the submit page
+        //Submit question button
         Button button1 = (Button)findViewById(R.id.button8);
+        /**
+         * This function runs when the submit button is clicked.
+         * Goes to separate page to submit a question.
+         */
         button1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                //go to submit page
                 Intent myIntent = new Intent(HomePage.this, SubmitPage.class);
                 startActivity(myIntent);
             }
         });
 
-        //when clicked, will take the user to the about page
+        //About page button
         Button button2 = (Button)findViewById(R.id.button7);
+        /**
+         * This function runs when the about page button is clicked.
+         * Goes to separate about page.
+         */
         button2.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //go to about page
@@ -95,40 +118,22 @@ public class HomePage extends Activity /*implements OnClickListener*/ {
             }
         });
 
-        ParseUser currentUser = ParseUser.getCurrentUser(); // Gets current user if they're logged in
-        if(currentUser != null){
-            //They're logged in
-            isRegistered = true;
-        } else {
-            //Not logged in
-            isRegistered = false;
-        }
-
-        View b = findViewById(R.id.registerButton); //Gets register button view
-        if(isRegistered){
-           b.setVisibility(View.GONE);  //If you are already registered, this removes the register button
-        }
-
-
-        Button registerButton = (Button)findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(new View.OnClickListener(){
+        Button button3 = (Button)findViewById(R.id.buttonLeader);
+        button3.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                //Go to register page
-                Intent myIntent = new Intent(HomePage.this, Register.class);
+                //go to leader board page
+                Intent myIntent = new Intent(HomePage.this, LeaderPage.class);
                 startActivity(myIntent);
-
             }
         });
-
-
-
-
 
     }
 
 
+    /**
+     * Creates menu bar on home page for user registration
+     */
     @Override
-    //creates menu bar on home page
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
